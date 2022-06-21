fe9e1b77c5256f5cb0fb82ed49379903ac46fc4d
Divyansh Singh <kalki@Divyanshs-MacBook-Air.local>
2022-06-22 04:16:49 +0530

    Solve linter issues

--- script.js
+++ script.js
@@ -1,12 +1,13 @@
-const hamburger = document.querySelector(".hamburger");
-const navPop = document.querySelector(".navPop");
+const hamburger = document.querySelector('.hamburger');
+const navPop = document.querySelector('.navPop');
 
-hamburger.addEventListener("click", () => {
-   hamburger.classList.toggle("active");
-   navPop.classList.toggle("active");  
-} )
-
-document.querySelectorAll(".nav-item").forEach(n => n.addEventListener("click", () => {
-   hamburger.classList.remove("active");
-   navPop.classList.remove("active");
-}))
+hamburger.addEventListener('click', () => {
+  hamburger.classList.toggle('active');
+  navPop.classList.toggle('active'); 
+});
+document.querySelectorAll('.nav-item').forEach(n => {
+   return n.addEventListener('click', () => {
+      hamburger.classList.remove("active");
+      navPop.classList.remove("active");
+   });
+})
